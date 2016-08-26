bb5eb32c8948976624724646fedfd4d2526944b6
cesarvh <cesarv.h@berkeley.edu>
2016-08-26 13:22:59 -0700

    Added Initial Version

---
+++ js/main.js
@@ -0,0 +1,64 @@
+toggle();
+
+function toggle() {
+	console.log("wahhh");
+	chrome.storage.sync.get("key", function(results){
+		var options = results.key;
+		for (var op in options) {
+			if (window.location.href.indexOf(op) > -1) { // well, now we know we're in one of the urls so now we kinda check
+				if (options[op] == true) {
+					disableScrollAndPrepare(op);
+				} 
+			}
+		}
+	});
+
+};
+
+function disableScrollAndPrepare(option) {
+	document.body.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+	var contentItem = "";
+	switch (option) {
+		case "facebook":
+			contentItem = "#contentArea";
+			break;
+		case "twitter":
+			contentItem = "#timeline";
+			break;
+		case "vine":
+			contentItem = ".posts-container ";
+			break;
+		case "reddit":
+			contentItem = ".content";
+			break;
+		case "instagram":
+			contentItem = "._qj7yb";
+			break;
+		case "tumblr":
+			contentItem = ".l-content clearfix";
+			break;
+		case "youtube":
+			contentItem = "#content";
+			break;
+		default:
+			console.log("Not Supported");
+	}
+	deleteElements(contentItem);
+	
+
+}
+
+function deleteElements(item) {
+	$(item).remove();
+}
+
+
+
+//function disableBlock() {
+//    document.body.style.display = '';
+//    document.body.style.overflow = '';
+//    location.reload();
+//}
+
+
---
+++ js/ops.js
@@ -0,0 +1,78 @@
+var buttons = {
+	"tumblr"    : false,
+	"facebook"  : false,
+	"twitter"   : false,
+	"instagram" : false,
+	"reddit"    : false,
+	"youtube"   : false,
+	"vine"      : false
+};
+
+
+window.onload = function() {
+	loadUserPrefs();
+//	console.log(loadUserPrefs());
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('button');
+	for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', toggle);
+  }
+
+	
+});
+
+//var obj = { varName : varValue };
+//var obj = {};
+//obj[varName] = varValue;
+
+
+function toggle(e) {
+//	loadUserPrefs();
+	var buttonId = e.target.id;
+	 // console.log(buttonId);
+	var currElem = document.getElementById(buttonId);
+	
+	if (buttons[buttonId] == false) {
+		buttons[buttonId] = true;
+		currElem.style.background = "green";
+		storeUserPrefs(buttons);
+	} else {
+		buttons[buttonId] = false;
+		currElem.style.background = "red";
+		storeUserPrefs(buttons);
+	}
+}
+
+function storeUserPrefs(prefs) {
+//	console.log(prefs);
+    var key = 'key';
+	chrome.storage.sync.set({key: prefs}, function() {
+	});
+}
+
+function loadUserPrefs() {
+	chrome.storage.sync.get("key", function(obj) {
+//		console.log("loaded: ", obj)
+		buttons = obj.key;
+		
+		var vuttons = document.querySelectorAll('button');
+		var vuttons = document.querySelectorAll('button');
+		for (var i = 0; i < vuttons.length; i++) {
+			var currButton = vuttons[i].getAttribute("id");
+			if (buttons[currButton] == true) {
+				document.getElementById(currButton).style.background = "green";
+			} else {
+				document.getElementById(currButton).style.background = "red";
+
+			}
+		}	
+//		return obj;
+		
+	});
+//	return obj;
+
+};
+	
